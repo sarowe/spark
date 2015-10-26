d2a51da9b149e864076311dd0cd9a38558469259
Steve Rowe <sarowe@gmail.com>
2015-10-26 11:57:38 -0400

    Add a new spark.ml transformer, which builds a custom Lucene analyzer via Params that specify CharFilters, a Tokenizer, and Token Filters. The string-typed input column is transformed into a sequence of tokens in the output column.  Adds a dependency on lucene-analyzers-common.jar and (indirectly) lucene-core.jar.

--- mllib/src/main/scala/org/apache/spark/ml/param/params.scala
+++ mllib/src/main/scala/org/apache/spark/ml/param/params.scala
@@ -317,6 +317,41 @@ class IntArrayParam(parent: Params, name: String, doc: String, isValid: Array[In
 }
 
 /**
+ * :: DeveloperApi ::
+ * Specialized version of [[Param[Array[Map[String,String]]]]] for Java.
+ */
+@DeveloperApi
+class StringStringMapArrayParam
+(parent: Params, name: String, doc: String, isValid: Array[Map[String,String]] => Boolean)
+  extends Param[Array[Map[String,String]]](parent, name, doc, isValid) {
+
+  def this(parent: Params, name: String, doc: String) =
+    this(parent, name, doc, ParamValidators.alwaysTrue)
+
+  /** Creates a param pair with a [[java.util.List]] of [[java.util.Map]] values
+    * (for Java and Python). */
+  def w(value: java.util.List[java.util.Map[String,String]]): ParamPair[Array[Map[String,String]]]
+   = w(value.asScala.map(_.asScala.toMap).toArray)
+}
+
+/**
+ * :: DeveloperApi ::
+ * Specialized version of [[Param[Map[String,String]]]] for Java.
+ */
+@DeveloperApi
+class StringStringMapParam
+(parent: Params, name: String, doc: String, isValid: Map[String,String] => Boolean)
+  extends Param[Map[String,String]](parent, name, doc, isValid) {
+
+  def this(parent: Params, name: String, doc: String) =
+    this(parent, name, doc, ParamValidators.alwaysTrue)
+
+  /** Creates a param pair with a [[java.util.Map]] value (for Java and Python). */
+  def w(value: java.util.Map[String,String]): ParamPair[Map[String,String]]
+    = w(value.asScala.toMap)
+}
+
+/**
  * :: Experimental ::
  * A param and its value.
  */
