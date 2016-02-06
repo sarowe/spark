2025c2bd3b7685d047c92f4ab1f3f32dad75cbb1
Steve Rowe <sarowe@gmail.com>
2016-02-05 20:26:34 -0500

    In JavaLuceneAnalyzerSuite:
    - Add test for a pre-built analyzer.
    - Switch from Lists.toArrayList() to Collections.singletonList() and Arrays.toList()

--- mllib/src/test/java/org/apache/spark/ml/feature/JavaLuceneAnalyzerSuite.java
+++ mllib/src/test/java/org/apache/spark/ml/feature/JavaLuceneAnalyzerSuite.java
@@ -17,7 +17,6 @@
 
 package org.apache.spark.ml.feature;
 
-import com.google.common.collect.Lists;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.sql.Row;
 import org.junit.After;
@@ -29,6 +28,8 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.SQLContext;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class JavaLuceneAnalyzerSuite {
@@ -53,11 +54,11 @@ public class JavaLuceneAnalyzerSuite {
         .setInputCol("rawText")
         .setOutputCol("tokens"); // Default analysis schema: StandardTokenizer + LowerCaseFilter
 
-    assertExpectedTokens(analyzer1, Lists.newArrayList(
+    assertExpectedTokens(analyzer1, Arrays.asList(
         new TokenizerTestData("Test for tokenization.", new String[]{"test", "for", "tokenization"}),
         new TokenizerTestData("Te,st. punct", new String[]{"te", "st", "punct"})));
 
-    assertExpectedTokens(analyzer1, Lists.newArrayList(
+    assertExpectedTokens(analyzer1, Arrays.asList(
         new TokenizerTestData("Test for tokenization.", new String[]{"test", "for", "tokenization"}),
         new TokenizerTestData("Te,st. punct", new String[]{"te", "st", "punct"})));
 
@@ -75,7 +76,7 @@ public class JavaLuceneAnalyzerSuite {
         "  'analyzer': 'StdTok_max10'\n" +
         "}]}\n");
     analyzer1.setAnalysisSchema(analysisSchema1);
-    assertExpectedTokens(analyzer1, Lists.newArrayList(
+    assertExpectedTokens(analyzer1, Arrays.asList(
         new TokenizerTestData("我是中国人。 １２３４ Ｔｅｓｔｓ ",
             new String[]{"我", "是", "中", "国", "人", "１２３４", "Ｔｅｓｔｓ"}),
         new TokenizerTestData("some-dashed-phrase", new String[]{"some", "dashed", "phrase"})));
@@ -98,7 +99,7 @@ public class JavaLuceneAnalyzerSuite {
         .setAnalysisSchema(analysisSchema2)
         .setInputCol("rawText")
         .setOutputCol("tokens");
-    assertExpectedTokens(analyzer2, Lists.newArrayList(
+    assertExpectedTokens(analyzer2, Arrays.asList(
         new TokenizerTestData("Test for tokenization.",
             new String[]{"Tes", "t", "for", "tok", "eni", "zat", "ion"}),
         new TokenizerTestData("Te,st.  punct", new String[]{"Te", "st", "pun", "ct"})));
@@ -129,7 +130,7 @@ public class JavaLuceneAnalyzerSuite {
         .setInputCol("rawText")
         .setOutputCol("tokens");
 
-    assertExpectedTokens(analyzer, Lists.newArrayList(
+    assertExpectedTokens(analyzer, Arrays.asList(
         new TokenizerTestData("Test for 9983, tokenization.", new String[]{"9983"}),
         new TokenizerTestData("Te,st. punct", new String[]{})));
 
@@ -154,7 +155,7 @@ public class JavaLuceneAnalyzerSuite {
         "}]}\n");
     analyzer.setAnalysisSchema(analysisSchema2);
 
-    assertExpectedTokens(analyzer, Lists.newArrayList(
+    assertExpectedTokens(analyzer, Collections.singletonList(
         new TokenizerTestData("<html><body>remove<b>me</b> but leave<div>the&nbsp;rest.</div></body></html>",
             new String[]{"but", "leave", "the", "rest"})));
   }
@@ -187,7 +188,7 @@ public class JavaLuceneAnalyzerSuite {
         .setAnalysisSchema(analysisSchema)
         .setInputCol("rawText")
         .setOutputCol("tokens");
-    assertExpectedTokens(analyzer, Lists.newArrayList(
+    assertExpectedTokens(analyzer, Arrays.asList(
         new TokenizerTestData("Harold's not around.", new String[]{"harold", "around"}),
         new TokenizerTestData("The dog's nose KNOWS!", new String[]{"dog", "nose", "knows"})));
   }
@@ -211,7 +212,7 @@ public class JavaLuceneAnalyzerSuite {
         .setAnalysisSchema(analysisSchema)
         .setInputCol("rawText")
         .setOutputCol("tokens");
-    assertExpectedTokens(analyzer, Lists.newArrayList(
+    assertExpectedTokens(analyzer, Arrays.asList(
         new TokenizerTestData("Click on https://www.google.com/#q=spark+lucene",
             new String[]{"Click", "on", "https://www.google.com/#q=spark+lucene"}),
         new TokenizerTestData("Email caffeine@coffee.biz for tips on staying@alert",
@@ -223,9 +224,28 @@ public class JavaLuceneAnalyzerSuite {
     LuceneAnalyzer analyzer = new LuceneAnalyzer()
         .setInputCols(new String[]{"rawText"})
         .setOutputCol("tokens");
-    assertExpectedTokens(analyzer, Lists.newArrayList(
+    assertExpectedTokens(analyzer, Collections.singletonList(
         new MV_TokenizerTestData(new String[] {"Harold's not around.", "The dog's nose KNOWS!"},
             new String[]{"harold's", "not", "around", "the", "dog's", "nose", "knows"})));
+  }
+
+  @Test
+  public void testPrebuiltAnalyzer() {
+    String analyzerConfig = json("{\n" +
+        "'schemaType': 'LuceneAnalyzerSchema.v1',\n" +
+        "'inputColumns': [{\n" +
+        "  'regex': '.+',\n" +
+        "  'analyzer': 'org.apache.lucene.analysis.core.WhitespaceAnalyzer'\n" +
+        "}]}\n");
+    LuceneAnalyzer analyzer = new LuceneAnalyzer()
+        .setAnalysisSchema(analyzerConfig)
+        .setInputCol("rawText")
+        .setOutputCol("tokens");
+
+    assertExpectedTokens(analyzer, Arrays.asList(
+        new TokenizerTestData("Test for tokenization.",
+            new String[] { "Test", "for", "tokenization." }),
+        new TokenizerTestData("Te,st. punct", new String[] { "Te,st.", "punct" })));
   }
 
   @Test
@@ -233,9 +253,9 @@ public class JavaLuceneAnalyzerSuite {
     LuceneAnalyzer analyzer1 = new LuceneAnalyzer()
         .setInputCols(new String[] {"rawText1", "rawText2"})
         .setOutputCol("tokens");
-    assertExpectedTokens(analyzer1, Lists.newArrayList(
+    assertExpectedTokens(analyzer1, Collections.singletonList(
         new SV_SV_TokenizerTestData("Harold's not around.", "The dog's nose KNOWS!",
-            new String[] {"harold's", "not", "around", "the", "dog's", "nose", "knows"})));
+            new String[]{"harold's", "not", "around", "the", "dog's", "nose", "knows"})));
 
     String analysisSchema = json("{\n" +
         "'schemaType': 'LuceneAnalyzerSchema.v1',\n" +
@@ -266,24 +286,24 @@ public class JavaLuceneAnalyzerSuite {
         .setAnalysisSchema(analysisSchema)
         .setInputCols(new String[] {"rawText1", "rawText2"})
         .setOutputCol("tokens");
-    assertExpectedTokens(analyzer2, Lists.newArrayList(
+    assertExpectedTokens(analyzer2, Collections.singletonList(
         new SV_SV_TokenizerTestData("Harold's NOT around.", "The dog's nose KNOWS!",
             new String[]{"harold's", "not", "around", "The", "dog's", "nose", "KNOWS"})));
 
-    assertExpectedTokens(analyzer2, Lists.newArrayList(
+    assertExpectedTokens(analyzer2, Collections.singletonList(
         new SV_MV_TokenizerTestData("Harold's NOT around.",
             new String[] {"The dog's nose KNOWS!", "Good, fine, great..."},
             new String[] {"harold's", "not", "around", "The", "dog's",
                 "nose", "KNOWS", "Good", "fine", "great"})));
 
-    assertExpectedTokens(analyzer2, Lists.newArrayList(
+    assertExpectedTokens(analyzer2, Collections.singletonList(
         new MV_MV_TokenizerTestData(new String[] {"Harold's NOT around.", "Anymore, I mean."},
             new String[] {"The dog's nose KNOWS!", "Good, fine, great..."},
             new String[] {"harold's", "not", "around", "anymore", "i", "mean",
                 "The", "dog's", "nose", "KNOWS", "Good", "fine", "great"})));
 
     analyzer2.setInputCols(new String[] {"rawText1", "rawText2", "rawText3"});
-    assertExpectedTokens(analyzer2, Lists.newArrayList(
+    assertExpectedTokens(analyzer2, Collections.singletonList(
         new SV_SV_SV_TokenizerTestData(
             "Harold's NOT around.", "The dog's nose KNOWS!", "<html><body>Content</body></html>",
             new String[]{"harold's", "not", "around", "The", "dog's", "nose", "KNOWS", "content"})));
@@ -314,12 +334,12 @@ public class JavaLuceneAnalyzerSuite {
     LuceneAnalyzer analyzer = new LuceneAnalyzer()
         .setInputCols(new String[] {"rawText1", "rawText2"})
         .setOutputCol("tokens");
-    assertExpectedTokens(analyzer, Lists.newArrayList(
+    assertExpectedTokens(analyzer, Collections.singletonList(
         new MV_MV_TokenizerTestData(rawText1, rawText2, tokens)));
 
     // Then transform with token prefixes
     analyzer.setPrefixTokensWithInputCol(true);
-    assertExpectedTokens(analyzer, Lists.newArrayList(
+    assertExpectedTokens(analyzer, Collections.singletonList(
         new MV_MV_TokenizerTestData(rawText1, rawText2, prefixedTokens)));
   }
 
